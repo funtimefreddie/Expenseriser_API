b55170b2bb59b7ade3f58f032ef025032a5f6607
Dan Otto <theDanOtto@gmail.com>
2015-02-18 15:02:52 +0800

    added create method

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ExpensesController < Api::V1::ApiController
   def index  
 
     if authenticate    
-
+        #byebug
         start_date = params[:start_date].respond_to?(:to_date) ? params[:start_date].to_date : @user.expenses.min_by(&:date).date
         end_date = params[:end_date].respond_to?(:to_date) ? params[:end_date].to_date : @user.expenses.max_by(&:date).date
         @expenses = @user.expenses_between_dates(start_date, end_date) 
@@ -18,5 +18,22 @@ class Api::V1::ExpensesController < Api::V1::ApiController
     end  
 
   end
+
+  def create
+    #date = params[:date]
+    #amount = 
+    Expense.create(date: params[:date], amount: params[:amount], user_id: @user.id)
+    
+    #byebug
+
+    
+  end
+
+  private
+  # def auth
+  #   return authenticate
+  # end
+
+
 
 end
