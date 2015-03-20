415f6d59a07f389a7103d151a515a511c6ecfce7
Freddie Waite <f.m.waite@gmail.com>
2015-03-20 19:02:42 +0530

    cleaning code

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,14 +1,20 @@
 class Api::V1::ApiController < ActionController::Base
 # Authentication and other filters implementation.
 
-  def authenticate
-    token = request.headers["token"]
-    name = request.headers["name"]
-    Rails.logger.info "Token : " + token
-    Rails.logger.info "Name : " + name
-    @user = User.where(name: name).first
-    @user.api_key.token == token 
+  # # returns boolean - true if we can authenticate
+  # def authenticate
+  #   # receive token and name
+  #   token = request.headers["token"]
+  #   name = request.headers["name"]
+
+  #   # add to logger
+  #   Rails.logger.info "Token : " + token
+  #   Rails.logger.info "Name : " + name
+
+  #   # search user and compare stored token to received token
+  #   @user = User.where(name: name).first
+  #   @user.api_key.token == token 
     
-  end
+  # end
 
 end
--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -2,28 +2,7 @@ class Api::V1::ExpensesController < Api::V1::ApiController
 
   # before_action :authenticate
 
-  def index
-    if auth_user
-      if Expense.user(@user).count ==0
-        render json: { message: "You have no data - please submit date via a POST request" }, status: 401
-      else
-        
-        start_date = params[:start_date].respond_to?(:to_date) ? params[:start_date].to_date : Expense.user(@user).min_by(&:date).date
-        end_date = params[:end_date].respond_to?(:to_date) ? params[:end_date].to_date : Expense.user(@user).max_by(&:date).date
-        category = params[:category]
-
-        if category == nil
-          @expenses = Expense.user(@user).after_date(start_date).before_date(end_date)
-        else
-          @expenses = Expense.user(@user).after_date(start_date).before_date(end_date).category(category)
-        end
-      end
-
-    else
-      return render json: { message: "Invalid Token", status: 400}, status: 400
-    end
-  end
-
+  # authenticate user - check if we can find their token
   def auth_user
     token = request.headers["token"]
     @api_found = ApiKey.find_by_token(token)
@@ -34,38 +13,62 @@ class Api::V1::ExpensesController < Api::V1::ApiController
     end
   end
 
-  def create
 
-    token = request.headers["token"]
-    # BROKEN IF API KEY IS NOT VALID.....
-    @api_found = ApiKey.find_by_token(token)
-    if @api_found
-      @user = User.find(@api_found.user_id)
+  # request to display expenses
+  def index
+
+    # authenticate request
+    if auth_user
+
+      # check user has data to show
+      if Expense.user(@user).count ==0
+        render json: { message: "You have no data - please submit date via a POST request" }, status: 401
+      else
+
+        # retrieve paramaters
+        # set start and end date to data min/max if not provided        
+        start_date = params[:start_date].respond_to?(:to_date) ? params[:start_date].to_date : Expense.user(@user).min_by(&:date).date
+        end_date = params[:end_date].respond_to?(:to_date) ? params[:end_date].to_date : Expense.user(@user).max_by(&:date).date
+        category = params[:category]
+
+        # request category if provided (uses scopes defined in Expense model)
+        if category == nil
+          @expenses = Expense.user(@user).after_date(start_date).before_date(end_date)
+        else
+          @expenses = Expense.user(@user).after_date(start_date).before_date(end_date).category(category)
+        end
+      end
+
     else
+      # request not authenticated
       return render json: { message: "Invalid Token", status: 400}, status: 400
     end
-    if !(token = request.headers["token"])
-      return render json: { message: "Missing Token", status: 400}
-    elsif !params[:amount]
-      return render json: { message: "No Amount Amount Supplied", status: 400}
-    elsif !params[:date]
-      return render json: { message: "No Date Supplied", status: 400} 
-    end
-    # always pass the token through as a header request...
-    
-
-
-    
-    #byebug
-    if params[:date] && params[:amount] && params[:category] && valid_date? && valid_amount?
-      @new_expense = Expense.create(category: params[:category], date: params[:date], amount: params[:amount], user_id: @user.id)
-      render json: { message: "Good going! You made an expense for $#{params[:amount]}", object: @new_expense }, status: 200
-    else
-      whats_not_supplied(params)     
-    end
   end
-  # you know the hardest part about breaking up with a Japanese girl? You have to drop the bomb twice before she gets it
 
+
+
+  # request to provide / submit expenses
+  def create
+
+    # authenticate request
+    if auth_user
+
+      #check we have everything we need!
+      if params[:date] && params[:amount] && params[:category] && valid_date? && valid_amount?
+        @new_expense = Expense.create(category: params[:category], date: params[:date], amount: params[:amount], user_id: @user.id)
+        render json: { message: "Good going! You made an expense for $#{params[:amount]}", object: @new_expense }, status: 200
+      else
+      # tell user what they haven't provided correctly
+        whats_not_supplied(params)     
+      end
+
+    else
+      # request not authenticated
+      return render json: { message: "Invalid Token", status: 400}, status: 400
+    end
+
+  end
+ 
   private
 
   def valid_date?
@@ -79,29 +82,19 @@ class Api::V1::ExpensesController < Api::V1::ApiController
   def whats_not_supplied(params)
     if !params[:date]
       return render json: { message: "You didn't supply a date. Expense not created", status: 400}
-      elsif !params[:amount]
+    elsif !params[:amount]
         return render json: { message: "You didn't supply an amount. Expense not created", status: 400}
-      elsif !params[:category]
+    elsif !params[:category]
         return render json: { message: "You didn't supply a category. Expense not created", status: 400}
-      elsif !@valid_date
+    elsif !@valid_date
         return render json: { message: "Incorrect form for Date. YYYY-MM-DD", status: 400}
-      else
+    else
         return render json: { message: "Incorrect format for Amount. Be sure to check that your Amount does not contain any letters.", status: 400}
       end
+  end  
+  
+  def expense_params
+    params.require(:team).permit(:user_id, :date, :amount, :category)
   end
-  # def auth
-  #   return authenticate
-  # end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def expense_params
-      params.require(:team).permit(:user_id, :date, :amount, :category)
-    end
-
-
 
 end
